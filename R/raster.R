feb3736a831a12b413371bfc4bc866e4ee488229
bbest <ben@ecoquants.com>
2018-12-14 16:17:55 -0800

    ran marxan and prioritizr, + pkgdown documentation

--- R/raster.R
+++ R/raster.R
@@ -31,3 +31,80 @@ rescale_grid <- function(r, res_new=0.1, fxn="ngb"){
     r <- raster::disaggregate(r, res_old/res_new)
   }
 }
+
+
+#' Quick leaflet map of raster
+#'
+#' @param r raster
+#' @param r_title title of raster layer
+#' @param na0 convert 0s to NA (default = FALSE)
+#' @param method interpolation method for web mercator, "ngb" (nearest neighbor) or "bilinear" (default "ngb")
+#' @param boundary_shp path to high seas boundary shapefile
+#'
+#' @return
+#' @export
+#'
+#' @examples
+qmap_r <- function(
+  r, r_title, na0=T, method="ngb",
+  boundary_shp="~/Google Drive/projects/bbnj/data/derived/boundary/high_seas_s05.shp"){
+  # b2_shp="~/Google Drive/projects/bbnj/data/derived/high-seas_boundary/high_seas_final_v2.shp"
+
+  # debug
+  # boundary_shp="~/Google Drive/projects/bbnj/data/derived/high-seas_boundary/high_seas_final.shp"
+  # r = r_sol; r_title = "Solution"
+  # r = raster("~/Google Drive/projects/bbnj/data/derived/biodiversity/spp_bivalves_be_0.5dd.tif")
+  # r_title = "bivalves"
+  # na0=T; method="ngb"
+
+  boundary <- read_sf(boundary_shp) %>%
+    lwgeom::st_make_valid() # plot(boundary)
+
+  if (na0) r[r==0] <- NA
+  #plot(r)
+
+  pal <- colorNumeric("Spectral", values(r), reverse=T, na.color = "transparent")
+
+  is_1val <- ifelse(length(unique(r)) == 1, T, F)
+  if (is_1val) pal <- colorNumeric("Reds", values(r), reverse=T, na.color = "transparent")
+
+  r
+
+  suppressWarnings({ # b/c addRasterImage() -> rgdal::rawTransform ... point(s) not finite
+  m <- leaflet(
+    options = leafletOptions(
+      worldCopyJump = T)) %>%
+    addProviderTiles(
+      providers$Esri.OceanBasemap,
+      options = providerTileOptions(opacity=0.8),
+      group = "Esri.OceanBasemap") %>%
+    addProviderTiles(
+      providers$Stamen.TonerLite,
+      options = providerTileOptions(opacity=0.8),
+      group = "Stamen.TonerLite") %>%
+    addRasterImage(
+      r, colors = pal, opacity=0.8, group=r_title, method=method) %>%
+    addPolygons(
+      data=boundary, fill=F, weight=2, group="high seas") %>%
+    addLayersControl(
+      baseGroups = c("Esri.OceanBasemap", "Stamen.TonerLite"),
+      overlayGroups = c(r_title, "high seas"))
+  })
+
+  if (is_1val){
+    m <- m %>%
+      addLegend(
+        group = r_title, colors = pal(unique(r)),
+        labels = unique(r),
+        position = "bottomright",
+        title = r_title)
+  } else {
+    m <- m %>%
+      addLegend(
+        group = r_title, pal = pal,
+        position = "bottomright",
+        values = values(r),
+        title = r_title)
+  }
+  m
+}
--- README.md
+++ README.md
@@ -6,7 +6,37 @@ This repository is for storing code, including:
 - R package of functions
 - Shiny app for interactive display
 
-## R package creation
+## Install R package
+
+From R console (such as in RStudio IDE):
+
+```r
+# install devtools if you need it
+# install.packages("devtools")
+
+# install package
+devtools::install_github("ecoquants/bbnj")
+
+# load library
+library(bbnj)
+
+# get help
+help(package="bbnj")
+```
+
+## Run Shiny app
+
+```r
+# install shiny if you need it
+# install.packages("shiny")
+
+# run shiny app
+shiny::runGitHub("ecoquants/bbnj", subdir="inst/app")
+```
+
+## Developer Notes
+
+### R package creation
 
 ```r
 usethis::create_package(path = "../bbnj")
@@ -17,6 +47,12 @@ devtools::check()
 
 ```r
 usethis::use_package("raster")
+usethis::use_package("sf")
 usethis::use_package("leaflet")
 ```
 
+### Documentation
+
+```r
+pkgdown::build_site()
+```
